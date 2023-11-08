26795fbeeaf876d590a2ebce882a44722c181e6c
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2023-11-08 12:18:52 -0600

    Update main.c

--- Data-Structures-&-Algorithms/Snakes-&-Ladders/main.c
+++ Data-Structures-&-Algorithms/Snakes-&-Ladders/main.c
@@ -1,9 +1,32 @@
+/*
+    Programa que simula el juego "Serpientes y Escaleras"
+    para dos jugadores
+    Algoritmos y Estructuras de Datos
+    Piña Del Valle José 
+*/
 
-#include <stdio.h>
+#include <math.h>
 
-int main()
-{
-    printf("Hello World");
+#include "libs/colas.h"
+#include "libs/pilas.h"
+#include "libs/listas.h"
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+#include <time.h>
 
-    return 0;
-}
+//Definición de constantes y variables globales ----------------------------------------------------
+
+typedef struct Move{
+    NodoL* ini;
+    NodoL* fin;
+}Move;
+
+
+#define tamtab 10   //El tablero será cuadrado
+#define J1C "\x1b[46m" //Color CYAN
+#define J2C "\x1b[44m" //Color AZUL
+
+int J1 = 0, J2 = 0; 
+
+//Encabezados de las funciones utilizadas ----------------------------------------------------------
