ceadf1a100010914f5f5d866520f370d2af62a74
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2023-11-27 22:14:59 -0600

    Update listas.c

--- Data-Structures-&-Algorithms/Snakes-&-Ladders/libs/listas.c
+++ Data-Structures-&-Algorithms/Snakes-&-Ladders/libs/listas.c
@@ -1,1 +1,10 @@
-##
+#include<stdlib.h>
+
+//Funciones básicas
+
+NodoL* CrearNodo(element e){
+    NodoL* nodo = (NodoL*)malloc(sizeof(NodoL));
+    nodo->e = e;
+    nodo->siguiente = NULL;
+    return nodo;
+}
