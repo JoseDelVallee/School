6c5fc3bd6a7e5d61a123610de84e0b49004f2bbe
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2023-12-05 23:53:16 -0600

    Update pilas.c

--- Data-Structures-&-Algorithms/Snakes-&-Ladders/libs/pilas.c
+++ Data-Structures-&-Algorithms/Snakes-&-Ladders/libs/pilas.c
@@ -1,1 +1,14 @@
-#hola
+//pilas.c
+
+#include <stdlib.h>
+#define ERRORP '*'
+
+//Funciones básicas
+
+Node* CrearNodoP(elementp e){
+    Node* nodo = (Node *)malloc(sizeof(Node));
+    //Datos
+    nodo->e = e;
+    nodo -> siguiente = NULL;
+    return nodo;
+}
