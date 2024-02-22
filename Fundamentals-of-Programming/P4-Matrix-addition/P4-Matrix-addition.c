f3bf60096d6be4fcbe30f6452795031e0427317a
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2024-02-21 23:40:50 -0600

    Update P4-Matrix-addition.c

--- Fundamentals-of-Programming/P4-Matrix-addition/P4-Matrix-addition.c
+++ Fundamentals-of-Programming/P4-Matrix-addition/P4-Matrix-addition.c
@@ -1,1 +1,14 @@
 #P4 Matrix-addition
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+	system("color F3");
+	int a[2][2] = {{8,7},{2,1}};
+	int b[2][2] = {{2,3},{9,4}};
+	int r[2][2] = {0};
+	int i,j;
+
+}
