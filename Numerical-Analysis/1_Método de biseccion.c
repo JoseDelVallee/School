e58035d76abac8c6e3fd937c1fe583f1c4c45b10
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2024-09-20 23:40:52 -0600

    Update 1_Método de biseccion.c

--- Numerical-Analysis/1_Método de biseccion.c
+++ Numerical-Analysis/1_Método de biseccion.c
@@ -4,3 +4,7 @@
 double funcionEcuacion(double x) {
     return x * x - 4;
 }
+
+void metodoBiseccion(double extremoIzquierdo, double extremoDerecho, double tolerancia, int numeroMaximoIteraciones) {
+    int iteracion;
+    double puntoMedio, valorEnExtremoIzquierdo, valorEnPuntoMedio;
