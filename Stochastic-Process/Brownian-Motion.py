0d00f8dc77a09f0f70c22c3338684954e0d87ed3
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2024-10-24 23:38:32 -0600

    Update Brownian-Motion.py

--- Stochastic-Process/Brownian-Motion.py
+++ Stochastic-Process/Brownian-Motion.py
@@ -4,3 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+
+# Parámetros de la simulación
+num_steps = 1000       # Número de pasos de la simulación
+step_size = 1          # Tamaño del paso (desviación estándar de los incrementos)
+time_range = 10        # Duración total del tiempo
+initial_position = 0   # Posición inicial de la partícula
+time = np.linspace(0, time_range, num_steps)  # Rango de tiempo
