cf914abd99ff2ce71f464c82fdf06914fee11222
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2024-11-13 11:19:15 -0600

    Update P3-Linear-Regression-BGD.py

--- Machine-Learning/P3-Linear-Regression-BGD.py
+++ Machine-Learning/P3-Linear-Regression-BGD.py
@@ -3,3 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+
+# Dataset y su visualización
+df = pd.read_csv("/content/drive/MyDrive/Prácticas Machine Learning/Práctica3-Regresión-Lineal-con-BGD-Machine-Learning/casas.csv")
+df
