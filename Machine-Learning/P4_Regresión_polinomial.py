d803798930e1699a8903a0805c431765a3323359
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-01-20 23:53:12 -0600

    Update P4_Regresión_polinomial.py

--- Machine-Learning/P4_Regresión_polinomial.py
+++ Machine-Learning/P4_Regresión_polinomial.py
@@ -8,3 +8,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, SGDRegressor
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, RobustScaler
 from sklearn.metrics import mean_squared_error, r2_score
+
+# Dataset y su visualización
+
+df = pd.read_csv('/content/drive/MyDrive/Prácticas Machine Learning/Práctica 4_Regresión_polinomial_Machine_Learning/datos.csv')
+df
