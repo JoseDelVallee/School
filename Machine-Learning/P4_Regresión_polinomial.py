e4c8aafb44e48a595c51ff754de57971b99291d0
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-01-20 23:53:54 -0600

    Update P4_Regresión_polinomial.py

--- Machine-Learning/P4_Regresión_polinomial.py
+++ Machine-Learning/P4_Regresión_polinomial.py
@@ -24,3 +24,6 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, shuffle
 # Entrenamos el modelo
 linear_OLS_model = LinearRegression()
 linear_OLS_model.fit(x_train, y_train)
+
+# Realizamos predicciones
+y_pred_linear_OLS_model = linear_OLS_model.predict(x_test)
