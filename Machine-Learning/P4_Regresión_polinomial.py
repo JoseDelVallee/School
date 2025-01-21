40e6353b719e67d774871bf3683b0900f067677f
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-01-20 23:52:59 -0600

    Update P4_Regresión_polinomial.py

--- Machine-Learning/P4_Regresión_polinomial.py
+++ Machine-Learning/P4_Regresión_polinomial.py
@@ -1,1 +1,10 @@
+# Importar Bibliotecas
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, RobustScaler
+from sklearn.metrics import mean_squared_error, r2_score
