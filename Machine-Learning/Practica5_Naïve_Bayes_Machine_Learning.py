bd85d7b24c934adc2f709c3c7c5ac2b96b2b3c72
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-03-21 19:31:25 -0600

    Update Practica5_Naïve_Bayes_Machine_Learning.py

--- Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
+++ Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
@@ -6,3 +6,7 @@ from sklearn.model_selection import train_test_split, KFold
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
+
+# Dataset y su visualización
+
+dfIris = pd.read_csv("/content/drive/MyDrive/Prácticas Machine Learning/Práctica5_Naïve Bayes_Machine_Learning/iris.csv")
