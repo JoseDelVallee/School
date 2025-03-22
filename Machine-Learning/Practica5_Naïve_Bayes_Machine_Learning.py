8366fc841068d3df06871c81920c64a70289cdf0
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-03-21 19:31:13 -0600

    Update Practica5_Naïve_Bayes_Machine_Learning.py

--- Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
+++ Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
@@ -4,3 +4,5 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.metrics import confusion_matrix, classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
