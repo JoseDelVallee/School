de3b8e0d568ced17c8b5f865b16952817d852ccc
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-03-26 00:20:30 -0600

    Update Practica5_Naïve_Bayes_Machine_Learning.py

--- Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
+++ Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
@@ -11,3 +11,5 @@ import matplotlib.pyplot as plt
 
 dfIris = pd.read_csv("/content/drive/MyDrive/Prácticas Machine Learning/Práctica5_Naïve Bayes_Machine_Learning/iris.csv")
 dfIris
+
+x, y = dfIris.drop("species", axis=1).values, dfIris["species"].values
