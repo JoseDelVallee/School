65644a8227fab85c7724be3623b985f5cb35bf9d
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-04-20 12:03:14 -0600

    Update Practica5_Naïve_Bayes_Machine_Learning.py

--- Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
+++ Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
@@ -97,3 +97,5 @@ matriz.plot()
 plt.show()
 
 # Dataset y su visualización
+dfEmails = pd.read_csv("/content/drive/MyDrive/Prácticas Machine Learning/Práctica5_Naïve Bayes_Machine_Learning/emails.csv").iloc[:, 1:]
+dfEmails
