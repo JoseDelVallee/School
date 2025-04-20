b9943cf9be5ab037960bd76162c379f0148cc8cd
Jose Del Valle <92696568+JoseDelVallee@users.noreply.github.com>
2025-04-20 12:03:24 -0600

    Update Practica5_Naïve_Bayes_Machine_Learning.py

--- Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
+++ Machine-Learning/Practica5_Naïve_Bayes_Machine_Learning.py
@@ -99,3 +99,5 @@ plt.show()
 # Dataset y su visualización
 dfEmails = pd.read_csv("/content/drive/MyDrive/Prácticas Machine Learning/Práctica5_Naïve Bayes_Machine_Learning/emails.csv").iloc[:, 1:]
 dfEmails
+x,y = dfEmails.drop("Prediction", axis=1).values, dfEmails["Prediction"].values
+x,y
